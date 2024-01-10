2094b7389d53bcc48d6939cce4f54d93a4d8903e
Margaret Wismer <margaretwismer@Margarets-Air.fios-router.home>
2024-01-09 21:51:40 -0500

    add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import './App.css'
+import rect1 from './assets/rect1.svg'
+import libertycard from './assets/libertycard.svg'
+
 
 function App() {
 
   return (
     <>
-
-      <h1>WELCOME TO PHILLY PARTY BRIDGE</h1>
-      <div className="card">
-        <h4>Relaxed games for enthusiastic players</h4>
-      </div>
-      <div>
-        <a href=" https://cloud1-fsar.nfdc.net" target="_blank">
-          Members login
-        </a>
-
+      <div style={{ 
+          backgroundImage: `URL(${libertycard})`,
+          height: 1100,
+          width: 800,
+        }}>
+          <h1 style={{opacity: 1}}>WELCOME TO PHILLY PARTY THE GAME</h1>
+          <div className="card">
+            <h4>Relaxed games for enthusiastic players</h4>
+          </div>
+          <div>
+            <a href=" https://cloud1-fsar.nfdc.net" target="_blank">
+              Members login
+            </a>
+          </div>
       </div>
     </>
   )
