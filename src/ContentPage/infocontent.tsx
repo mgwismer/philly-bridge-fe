1dbc43673a26ed27a3877e76c6c434f433e7a1df
Margaret Wismer <margaretgwismer@gmail.com>
2024-10-10 18:22:26 -0400

    Gh 8 garys suggestions (#10)
    * change front page
    
    * rm lines
    
    ---------
    
    Co-authored-by: Margaret Wismer <margaretwismer@Margarets-Air.fios-router.home>

--- src/ContentPage/infocontent.tsx
+++ src/ContentPage/infocontent.tsx
@@ -7,23 +7,32 @@ export const InfoContent: React.FC = () => {
         <div className="content-container">
             <div className="content-intro-message-container">
             <div className="sub-content-container">
-                <p className="info-header"> Who we are </p>
+                <p className="info-header-welcome"> Hi, you found us! Welcome to Philly Bridge Players </p>
                 <p className="info-message">
-                A group of intermediate to advanced players gathers weekly 
-                to play Bridge in a relaxed, social atmosphere. We’ll bring the cards and will be in the back. Partnerships welcome but not required. We will break partnerships up, at least temporarily, if we don’t have a full table to allow everyone a chance to play and to keep wait time to a minimum.
-                Most of us use the standard ACBL yellow card bidding convention as a baseline. 
+                    We are a group of bridge players who meet weekly to play and improve our games. Mostly
+                    we have fun and enjoy each other's company
                 </p>
             </div>
             <div className="sub-content-container">
-                <p className="info-header"> Where we are </p>
+                <p className="info-header-describe"> You might consider joining us if you are </p>
+                <ul>
+                    <li> An intermediate player interested in meeting new players </li>
+                    <li> A duplicate pair interested in practicing without paying card fees </li>
+                </ul>
                 <p className="info-message">
-                Panera Bread@Presidential Blvd, 3900 City Ave. Philadelphia, PA
+                You can join us as a pair or as a single player
+                <br/>
+                We are not able to accommodate beginners but, if you are a new player, taking lessons with
+                a partner, we would be happy for you to try our group.
                 </p>
+
             </div>
             <div className="sub-content-container">
-                <p className="info-header"> When we are </p>
+              <p className="info-message">
+                Where: Panera Bread@Presidential Blvd, 3900 City Ave. Philadelphia, PA
+                </p>
                 <p className="info-message">
-                Saturday Mornings: 10AM-1PM
+                When: Saturday Mornings: 10AM-1PM
                 </p>
             </div>
             <div className="sub-content-container">
