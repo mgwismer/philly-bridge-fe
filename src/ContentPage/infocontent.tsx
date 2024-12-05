39268cc0554cc044d57f60f79a2c38d4660dd5d8
Margaret Wismer <margaretwismer@Margarets-Air.fios-router.home>
2024-12-05 16:50:17 -0500

    add announcements

--- src/ContentPage/infocontent.tsx
+++ src/ContentPage/infocontent.tsx
@@ -8,6 +8,7 @@ export const InfoContent: React.FC = () => {
             <div className="content-intro-message-container">
             <div className="sub-content-container">
                 <p className="info-header-welcome"> Hi, you found us! Welcome to Philly Bridge Players </p>
+                <p className="info-header"> WHO WE ARE </p>
                 <p className="info-message">
                     We are a group of bridge players who meet weekly to play and improve our games. Mostly
                     we have fun and enjoy each other's company
@@ -19,6 +20,7 @@ export const InfoContent: React.FC = () => {
                     <li> An intermediate player interested in meeting new players </li>
                     <li> A duplicate pair interested in practicing without paying card fees </li>
                 </ul>
+                <p className="info-header"> WHERE </p>
                 <p className="info-message">
                 You can join us as a pair or as a single player
                 <br/>
@@ -31,14 +33,23 @@ export const InfoContent: React.FC = () => {
               <p className="info-message">
                 Where: Panera Bread@Presidential Blvd, 3900 City Ave. Philadelphia, PA
                 </p>
+                <p className="info-header"> WHEN </p>
                 <p className="info-message">
                 When: Saturday Mornings: 10AM-1PM
                 </p>
             </div>
             <div className="sub-content-container">
-                <p className="info-header"> For more info contact </p>
+                <p className="info-header"> COST </p>
                 <p className="info-message">
-                    phillypartybridge@gmail.com
+                   There is no cost to play with us a few times. If you decide to join us more regularly there is
+                   a modest annual fee.
+                </p>
+            </div>
+            <div className="sub-content-container">
+                <p className="info-header"> NEXT </p>
+                <p className="info-message">
+                    If you would like to come play or would like more information email us at
+                    phillypartybridge@gmail.com.
                 </p>
             </div>
             </div>
